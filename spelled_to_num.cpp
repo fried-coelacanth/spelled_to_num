24126fa86e6770da7fcd65783a1eb6a22f29452a
Razor <eric.razorjr@gmail.com>
2026-06-23 21:09:11 -0400

    Initial CMake commit

---
+++ spelled_to_num.cpp
@@ -0,0 +1,12 @@
+﻿// spelled_to_num.cpp : Defines the entry point for the application.
+//
+
+#include "spelled_to_num.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
