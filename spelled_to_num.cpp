677c287cff110c19e393d27fb2b08f083fd900af
Razor <8428193+fried-coelacanth@users.noreply.github.com>
2026-06-25 12:44:32 -0400

    Convert spelled-out words to digits

--- spelled_to_num.cpp
+++ spelled_to_num.cpp
@@ -1,9 +1,37 @@
-﻿// spelled_to_num.cpp : Defines the entry point for the application.
-//
-
-#include "spelled_to_num.h"
+﻿#include "spelled_to_num.h"
 
 int main()
 {
+	std::string current;
+	std::cout << "Spell some numbers: ";
+
+	while (std::cin >> current)
+	{
+		if (current == "zero")
+		{
+			std::cout << "0";
+		}
+
+		if (current == "one")
+		{
+			std::cout << "1";
+		}
+
+		if (current == "two")
+		{
+			std::cout << "2";
+		}
+
+		if (current == "three")
+		{
+			std::cout << "3";
+		}
+
+		if (current == "four")
+		{
+			std::cout << "4";
+		}
+	}
+
 	return 0;
 }
